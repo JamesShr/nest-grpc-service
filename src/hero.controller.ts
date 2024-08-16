f7be831a7e56a47161f8f89e9ae45d5dfec425a5
JamesShi <jamesshr12345@gmail.com>
2024-08-16 18:17:58 +0800

    first commit

---
+++ src/hero.controller.ts
@@ -0,0 +1,27 @@
+import { Metadata, ServerUnaryCall } from '@grpc/grpc-js';
+import { Controller } from '@nestjs/common';
+import { GrpcMethod } from '@nestjs/microservices';
+
+class HeroById {
+  id: number;
+}
+
+class Hero {
+  id: number;
+  name: string;
+}
+@Controller()
+export class HeroesController {
+  @GrpcMethod('HeroesService', 'FindOne')
+  findOne(
+    data: HeroById,
+    metadata: Metadata,
+    call: ServerUnaryCall<any, any>,
+  ): Hero {
+    const items = [
+      { id: 1, name: 'John' },
+      { id: 2, name: 'Doe' },
+    ];
+    return items.find(({ id }) => id === data.id);
+  }
+}
